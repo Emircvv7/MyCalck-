4002a7f9be0dbf2f6837e898f344cdd0e64ef256
Emircvv7 <spike10lvl@gmail.com>
2024-01-31 20:51:46 +0600

    Initial commit

---
+++ app/src/main/java/com/example/mycalck/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.mycalck
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.mycalck.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    private val viewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        with(binding) {
+            btnInc.setOnClickListener {
+                viewModel.inc()
+            }
+
+            btnDec.setOnClickListener {
+                viewModel.dec()
+            }
+
+            viewModel.counterLv.observe(this@MainActivity) {
+                tvResult.text = it.toString()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mycalck/MainViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.mycalck
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    private var count = 0
+    private val _counterLv = MutableLiveData<Int>()
+    val counterLv: LiveData<Int> get() = _counterLv
+
+    fun inc() {
+        _counterLv.value = ++count
+    }
+
+    fun dec() {
+        _counterLv.value = --count
+    }
+
+    fun getCount() = _counterLv.value
+}
+
